592516b72f9599ac95b2191921761eb0593ba1b0
ocsfwarch <38299239+ocsfwarch@users.noreply.github.com>
2021-11-17 14:40:10 -0800

    Adds styling to the home view

--- src/components/controls/CreateDeckBtn.js
+++ src/components/controls/CreateDeckBtn.js
@@ -3,12 +3,14 @@ import { Link } from "react-router-dom";
 
 const CreateDeckBtn = () => {
   return (
-    <Link to="/decks/new">
-      <button type="button" className="btn btn-primary">
-        <span className="oi oi-plus" />
-        <span className="pl-3 font-weight-bold">Create Deck</span>
-      </button>
-    </Link>
+    <div className="create-deck-btn">
+      <Link to="/decks/new">
+        <button type="button" className="btn btn-primary">
+          <span className="oi oi-plus" />
+          <span className="pl-3 font-weight-bold">Create Deck</span>
+        </button>
+      </Link>
+    </div>
   );
 };
 
--- src/components/deck/Deck.js
+++ src/components/deck/Deck.js
@@ -3,52 +3,58 @@ import { Link } from "react-router-dom";
 
 const Deck = ({ deck, handleDeckDelete, showTotal }) => {
   return (
-    <div className="base-container">
-      <div>
-        <span>{deck.name}</span>{" "}
-        <span>{showTotal ? `${deck.cards.length} cards` : ""}</span>
-      </div>
-      <div>{deck.description}</div>
-      <div>
-        {showTotal ? (
-          <Link to={`/decks/${deck.id}`}>
-            <button type="button" className="btn btn-secondary">
-              <span className="oi oi-eye" />
-              <span className="pl-3 font-weight-bold">View</span>
+    <div className="row mb-3">
+      <div className="col">
+        <div className="card add-shadow">
+          <div className="card-header">
+            <div className="card-name-display">
+              <span className="title">{deck.name}</span>
+              <span>{showTotal ? `${deck.cards.length} cards` : ""}</span>
+            </div>
+          </div>
+          <div className="card-body card-text">{deck.description}</div>
+          <div className="card-footer">
+            {showTotal ? (
+              <Link to={`/decks/${deck.id}`}>
+                <button type="button" className="btn btn-secondary mr-1">
+                  <span className="oi oi-eye" />
+                  <span className="pl-3 font-weight-bold">View</span>
+                </button>
+              </Link>
+            ) : (
+              <Link to={`/decks/${deck.id}/edit`}>
+                <button type="button" className="btn btn-warning mr-1">
+                  <span className="oi oi-pencil" />
+                  <span className="pl-3 font-weight-bold">Edit</span>
+                </button>
+              </Link>
+            )}
+            <Link to={`/decks/${deck.id}/study`}>
+              <button type="button" className="btn btn-primary mr-1">
+                <span className="oi oi-book" />
+                <span className="pl-3 font-weight-bold">Study</span>
+              </button>
+            </Link>
+            {!showTotal ? (
+              <Link to={`/decks/${deck.id}/cards/new`}>
+                <button type="button" className="btn btn-success mr-1">
+                  <span className="oi oi-plus" />
+                  <span className="pl-3 font-weight-bold">Add Cards</span>
+                </button>
+              </Link>
+            ) : (
+              ""
+            )}
+            <button
+              type="button"
+              className="btn btn-danger"
+              onClick={() => handleDeckDelete(deck.id)}
+            >
+              <span className="oi oi-trash" />
+              <span className="pl-3 font-weight-bold">Delete</span>
             </button>
-          </Link>
-        ) : (
-          <Link to={`/decks/${deck.id}/edit`}>
-            <button type="button" className="btn btn-warning">
-              <span className="oi oi-pencil" />
-              <span className="pl-3 font-weight-bold">Edit</span>
-            </button>
-          </Link>
-        )}
-        <Link to={`/decks/${deck.id}/study`}>
-          <button type="button" className="btn btn-primary">
-            <span className="oi oi-book" />
-            <span className="pl-3 font-weight-bold">Study</span>
-          </button>
-        </Link>
-        {!showTotal ? (
-          <Link to={`/decks/${deck.id}/cards/new`}>
-            <button type="button" className="btn btn-success">
-              <span className="oi oi-plus" />
-              <span className="pl-3 font-weight-bold">Add Cards</span>
-            </button>
-          </Link>
-        ) : (
-          ""
-        )}
-        <button
-          type="button"
-          className="btn btn-danger"
-          onClick={() => handleDeckDelete(deck.id)}
-        >
-          <span className="oi oi-trash" />
-          <span className="pl-3 font-weight-bold">Delete</span>
-        </button>
+          </div>
+        </div>
       </div>
     </div>
   );
