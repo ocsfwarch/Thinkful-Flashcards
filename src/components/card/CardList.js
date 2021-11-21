205f5c92f7b06e8ec413736959abfbf5bc8835f5
ocsfwarch <38299239+ocsfwarch@users.noreply.github.com>
2021-11-21 14:52:25 -0800

    Removes conditional logic in the return.

--- src/components/card/CardList.js
+++ src/components/card/CardList.js
@@ -1,19 +1,21 @@
 import Card from "./Card";
 
 const CardList = ({ deck, handleCardDelete }) => {
+  const display =
+    deck && deck.cards && deck.cards.length
+      ? deck.cards.map((card) => (
+          <Card
+            key={card.id}
+            deckId={card.deckId}
+            card={card}
+            handleCardDelete={handleCardDelete}
+          />
+        ))
+      : "No cards have been created.";
   return (
     <section>
       <h1>Cards</h1>
-      {deck && deck.cards && deck.cards.length
-        ? deck.cards.map((card) => (
-            <Card
-              key={card.id}
-              deckId={card.deckId}
-              card={card}
-              handleCardDelete={handleCardDelete}
-            />
-          ))
-        : " "}
+      {display}
     </section>
   );
 };
